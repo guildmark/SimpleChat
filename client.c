48a107694ba97d8407dd6d73a44655fd39eebc8c
guildmark <guildmark@gmail.com>
2021-04-26 20:51:16 +0200

    Start file

---
+++ client.c
@@ -0,0 +1,15 @@
+/*A program that acts as a simple chat, where the goal is for two terminal
+windows to chat with each other */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void) {
+
+	printf("Hello world!\n");
+
+	
+
+	return 0;
+}
